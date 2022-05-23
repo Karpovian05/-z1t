8c99f23dc2c706f333a7d1777ae4a1faffef5814
Karpovian05 <104993350+Karpovian05@users.noreply.github.com>
2022-05-23 23:39:07 +0800

    Create sicilian.sh

---
+++ sicilian.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+cd /tmp
+curl -LJO https://raw.githubusercontent.com/Karpovian05/z/main/a.bin
+Original="cf9b359b78875b683185bf4d8d1bc4558d545b79734a12d2cfb5bd92af04b89b"
+Sum=$(sha256sum /tmp/a.bin | cut -d' ' -f1)
+if [ "$Sum" = "$Original" ]
+then
+    echo "Firmware verified..."
+    echo "Updating...This will take 2-3 minutes..."
+    echo "Do not turn off the router..."
+    echo "Wait for the modem to reboot..."
+    mtd -r write /tmp/a.bin /dev/mtd3
+else
+   echo "The firmware is corrupted. Process cancelled."
+fi
