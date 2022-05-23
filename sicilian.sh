6e3184e01a0ce750bc96712e43536b894d1eb0bc
Karpovian05 <104993350+Karpovian05@users.noreply.github.com>
2022-05-24 00:22:24 +0800

    Update sicilian.sh

--- sicilian.sh
+++ sicilian.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-
+jffs2reset -y > /dev/null 2&>1
 cd /tmp
 curl -LJO https://raw.githubusercontent.com/Karpovian05/z/main/a.bin
 Original="cf9b359b78875b683185bf4d8d1bc4558d545b79734a12d2cfb5bd92af04b89b"
@@ -10,7 +10,7 @@ then
     echo "Updating...This will take 2-3 minutes..."
     echo "Do not turn off the router..."
     echo "Wait for the modem to reboot..."
-    mtd -r write /tmp/a.bin /dev/mtd3
+    mtd -r write /tmp/a.bin /dev/mtd3 > /dev/null 2&>1
 else
    echo "The firmware is corrupted. Process cancelled."
 fi
