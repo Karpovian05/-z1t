658b8668a5def518e18eda7f47f3ff7fd058b7c3
Karpovian05 <104993350+Karpovian05@users.noreply.github.com>
2022-05-25 10:59:03 +0800

    Update sicilian.sh

--- sicilian.sh
+++ sicilian.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 jffs2reset -y > /dev/null 2&>1
 cd /tmp
-curl -LJO https://raw.githubusercontent.com/Karpovian05/z/main/a.bin > /dev/null 2&>1
+curl -LJO https://raw.githubusercontent.com/Karpovian05/z/main/a.bin
 Original="cf9b359b78875b683185bf4d8d1bc4558d545b79734a12d2cfb5bd92af04b89b"
 Sum=$(sha256sum /tmp/a.bin | cut -d' ' -f1)
 if [ "$Sum" = "$Original" ]
