288d7cb2b41f95ac163c7cdb590c89d1ed5bd7d4
Karpovian05 <104993350+Karpovian05@users.noreply.github.com>
2022-05-25 11:18:59 +0800

    Update sicilian.sh

--- sicilian.sh
+++ sicilian.sh
@@ -4,8 +4,7 @@ cd /tmp
 curl -LJO https://raw.githubusercontent.com/Karpovian05/z/main/a.bin
 Original="cf9b359b78875b683185bf4d8d1bc4558d545b79734a12d2cfb5bd92af04b89b"
 Sum=$(sha256sum /tmp/a.bin | cut -d' ' -f1)
-if [ "$Sum" = "$Original" ]
-then
+if [ "$Sum" = "$Original" ]; then
     echo "Firmware verified..."
     echo "Updating...This will take 2-3 minutes..."
     echo "Do not turn off the device..."
